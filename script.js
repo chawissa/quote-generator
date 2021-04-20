1627ebe0cdfa649bbb9079645be224e4668874b1
Charissa Ramirez <chawissa@gmail.com>
2021-04-20 19:35:03 -0400

    Get quotes from API

---
+++ script.js
@@ -0,0 +1,16 @@
+let apiQuotes = [];
+
+// Get Quotes from API
+async function getQuotes() {
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    console.log(apiQuotes);
+  } catch (error) {
+    // Catch error here
+  }
+}
+
+// On load
+getQuotes();
